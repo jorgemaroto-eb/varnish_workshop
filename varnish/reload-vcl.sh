a58999621139413b928275f26f9f7afab848508e
Jorge Maroto <jorge.maroto@eventbrite.com>
2021-10-20 20:51:06 +0200

    comment

--- varnish/reload-vcl.sh
+++ varnish/reload-vcl.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# https://github.com/eventbrite/varnish_tests/blob/main/config/reload-vcl
+
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1) \
     && varnishadm vcl.load $NEW_UUID /etc/varnish/default.vcl \
     && varnishadm vcl.use $NEW_UUID \
