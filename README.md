37772f7263ac049240f6faaca65d4312512c77eb
Jorge Maroto <jorge.maroto@eventbrite.com>
2021-10-27 13:35:22 +0200

    Hello world

--- README.md
+++ README.md
@@ -2,20 +2,16 @@
 
 ### Using CURL:
 - `curl -sI http://localhost:8080/`: without adding headers.
-- `curl -sI http://localhost:8080/ttl/2`: add the `Cache-control` header with TTL for 2 seconds.
-
-### Check with the browser
-- Simple page: http://localhost:8080/simple/3
-- Splitting the page:
-    - Only content: http://localhost:8080/page/content/4
-    - Load using an iframe: http://localhost:8080/iframe/3
-    - Load using ESI: http://localhost:8080/esi/3
 
 ### Some commands
 ```bash
 docker-compose up --build
 
-docker-compose exec varnish varnishlog -g raw -i VCL_Log
+docker-compose exec varnish bash
+# inside of the container:
+varnishstat
+varnishadm backend.list
+varnishlog -i ReqUrl -i VCL_call -i VCL_return -q "VCL_call eq 'MISS'"
 
-docker-compose exec varnish reload-vcl.sh
+varnishtop -i ReqUrl -q "VCL_call eq 'MISS'"
 ```
--- apps/app.py
+++ apps/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, make_response, render_template
-from datetime import datetime
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -7,53 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World!"
-
-
-@app.route("/ttl/<int:ttl>")
-def ttl(ttl):
-    now = datetime.now().isoformat()
-
-    response = make_response(f"Setting TTL to {ttl}.\nNow it's {now}.")
-    response.headers["Cache-control"] = f"public, max-age={ttl}"
-
-    return response
-
-###########################################################################
-
-# Using a browser
-@app.route("/simple/<int:ttl>")
-def simple(ttl):
-    now = datetime.now().isoformat()
-
-    response = make_response(render_template("simple.html", now=now, ttl=ttl))
-    response.headers["Cache-control"] = "no-cache"
-    return response
-
-# SPLITTING THE PAGE
-
-@app.route("/page/content/<int:ttl>")
-def only_content(ttl):
-    now = datetime.now().isoformat()
-
-    response = make_response(render_template("_content.html", now=now, ttl=ttl))
-    response.headers["Cache-control"] = f"public, max-age={ttl}"
-
-    return response
-
-@app.route("/iframe/<int:ttl>")
-def using_iframe(ttl):
-    now = datetime.now().isoformat()
-
-    response = make_response(render_template("using_iframe.html", now=now, ttl=ttl))
-    response.headers["Cache-control"] = f"public, max-age={ttl}"
-
-    return response
-
-@app.route("/esi/<int:ttl>")
-def using_esi(ttl):
-    now = datetime.now().isoformat()
-
-    response = make_response(render_template("using_esi.html", now=now, ttl=ttl))
-    response.headers["Cache-control"] = f"public, max-age={ttl}"
-
-    return response
